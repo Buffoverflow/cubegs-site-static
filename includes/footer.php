2a33093b6e3ac54c17469f72edf63d5f6d65353f
Francisco <juniordez@gmail.com>
2024-12-08 17:18:05 -0300

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,15 @@
+
+
+    <!-- Footer -->
+    <footer id="anita-footer">
+        <div class="anita-footer-inner">
+            <div class="anita-copyright anita-js-copyright"></div>
+        </div>
+    </footer>
+
+    <!-- JS Scripts -->
+    <script src="js/lib/jquery.min.js"></script>
+    <script src="js/lib/aos.min.js"></script>
+    <script src="js/core.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>CubeGS | Team Portfolio | <?= $gameName ?></title>
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600;800&family=Rajdhani:wght@700&display=swap" rel="stylesheet">
+    <!-- Template Config -->
+    <link type="text/css" rel="stylesheet" href="css/config.css">
+    <!-- Libraries -->
+    <link type="text/css" rel="stylesheet" href="css/libs.css">
+    <!-- Template Styles -->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <!-- Responsive -->
+    <link type="text/css" rel="stylesheet" href="css/responsive.css">
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/favicon.png" sizes="32x32" />
+</head>
+<body>
+    <!-- Header -->
+    <header id="anita-header" class="anita-header">
+        <div class="anita-header-inner">
+            <div class="anita-logo-wrapper">
+                <!-- Logo -->
+                <a href="index.html" class="anita-logo is-retina">
+                    <img src="img/logo.png" alt="Cube Game Studio - Team Portfolio" width="192" height="80">
+                </a>
+            </div>
+        </div>
+    </header>
